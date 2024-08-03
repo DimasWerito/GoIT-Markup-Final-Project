d656c215dd568d5bf7747d9df67508a43d449794
DimasWerito <155931901+DimasWerito@users.noreply.github.com>
2024-08-03 14:42:11 +0000

    Deploying to gh-pages from @ DimasWerito/GoIT-Markup-Final-Project@5f9bed2cca5da094b39df1e4df6345dd69a88f6a 🚀

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 let modalEl = null;
+let backdropEl = null;
 
 const toggleMenu = (state = '') => {
-    if ('' === state) modalEl.classList.toggle('is-open');
-    else if ('open' === state) modalEl.classList.add('is-open');
-    else if ('close' === state) modalEl.classList.remove('is-open');
+    if (!modalEl.classList.contains('is-open') || 'open' === state) {
+        modalEl.classList.add('is-open');
+        backdropEl.addEventListener('click', parseClick);
+        document.addEventListener('keydown', parseKeydown);
+    } else {
+        modalEl.classList.remove('is-open');
+        backdropEl.removeEventListener('click', parseClick);
+        document.removeEventListener('keydown', parseKeydown);
+    }
+};
+
+const parseKeydown = event => {
+    if (event.code === 'Escape') toggleMenu('close');
+};
+
+const parseClick = event => {
+    if (event.target === backdropEl) toggleMenu('close');
+
+    const closestLink = event.target.closest('a');
+
+    if (closestLink?.classList.contains('nav-link')) toggleMenu('close');
+    if (closestLink?.classList.contains('soc-link')) toggleMenu('close');
 };
 
 const main = () => {
+    backdropEl = document.querySelector('.menu-backdrop');
     modalEl = document.querySelector('.menu-backdrop>.menu');
 };
 
