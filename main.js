522f1a944937fc856b88155c9d0246e3026a666d
DimasWerito <155931901+DimasWerito@users.noreply.github.com>
2024-08-04 07:10:43 +0000

    Deploying to gh-pages from @ DimasWerito/GoIT-Markup-Final-Project@df9b36a341473ce315cb1dbb6d8e4cbe5a158499 🚀

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-let modalEl = null;
-let backdropEl = null;
-
 const toggleMenu = (state = '') => {
+    const backdropEl = document.querySelector('.menu-backdrop');
+    const modalEl = document.querySelector('.menu-backdrop>.menu');
+
     if (!modalEl.classList.contains('is-open') || 'open' === state) {
         modalEl.classList.add('is-open');
         backdropEl.addEventListener('click', parseClick);
@@ -18,17 +18,10 @@ const parseKeydown = event => {
 };
 
 const parseClick = event => {
-    if (event.target === backdropEl) toggleMenu('close');
+    if (event.target === document.querySelector('.menu-backdrop')) toggleMenu('close');
 
     const closestLink = event.target.closest('a');
 
     if (closestLink?.classList.contains('nav-link')) toggleMenu('close');
     if (closestLink?.classList.contains('soc-link')) toggleMenu('close');
 };
-
-const main = () => {
-    backdropEl = document.querySelector('.menu-backdrop');
-    modalEl = document.querySelector('.menu-backdrop>.menu');
-};
-
-document.addEventListener('DOMContentLoaded', main);
